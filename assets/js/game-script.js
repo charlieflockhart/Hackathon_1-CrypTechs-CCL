73c41e5c8972f0d4531a3809ba10cf2df2fc919f
charlieflockhart <80785898+charlieflockhart@users.noreply.github.com>
2024-12-13 18:35:35 +0000

    Index.js - Cached - Game-script.js

---
+++ assets/js/game-script.js
@@ -0,0 +1,62 @@
+console.log('Testing game-script.js!');
+
+let theme = localStorage.getItem('theme');
+let difficulty = localStorage.getItem('difficulty');
+
+
+console.log('Theme:', theme);
+console.log('Difficulty:', difficulty);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Theme
+
+// let theme = 'movies';
+
+// document.getElementById('christmas-dropdown-button').onclick = () => {
+//     theme = 'christmas';    
+//     window.location.search = '?theme=christmas';
+// };
+
+// document.getElementById('movies-dropdown-button').onclick = () => {
+//     theme = 'movies';    
+//     window.location.search = '?theme=movies';
+// };
+
+// document.getElementById('food-dropdown-button').onclick = () => {
+//     theme = 'food';    
+//     window.location.search = '?theme=food';
+// };
+
+// document.getElementById('tech-dropdown-button').onclick = () => {
+//     theme = 'tech';    
+//     window.location.search = '?theme=tech';
+// };
+
+// document.getElementById('geography-dropdown-button').onclick = () => {
+//     theme = 'geography';    
+//     window.location.search = '?theme=geography';
+// };
+
+// if (window.location.search.includes('theme=christmas')) {
+//     theme = 'christmas';
+// }
+
+// if (window.location.search.includes('theme=movies')) {
+//     theme = 'movies';
+// }
+
+// if (window.location.search.includes('theme=food')) {
+//     theme = 'food';
+// }
+
+// if (window.location.search.includes('theme=tech')) {
+//     theme = 'tech';
+// }
+
+// if (window.location.search.includes('theme=geography')) {
+//     theme = 'geography';
+// }
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
